284e9ea312537532e6ed5b4bb033fcc83ee9c933
JG <joyce.ghantous@inria.fr>
2026-02-20 16:51:48 +0100

    del run.sh

--- run.sh
+++
@@ -1,9 +0,0 @@
-cd $HOME/codes/hgo_biomechanical_cardiac_model/  
-
-edp="Resolution_2d_LR2.edp"
-args=(2009 2025)
-
-for a in "${args[@]}"; do
-  echo "=== Running: FreeFem++ $edp $a ==="
-  FreeFem++ "$edp" "$a"  > "Curta/logs/run_${a}.out"
-done
